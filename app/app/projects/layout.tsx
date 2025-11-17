92d1a6bba4d648ca6450909455e3e289f3979144
Sudeep Shetty <shettysudeep1010@gmail.com>
2025-11-18 02:15:51 +0530

    Update layout component to use min-h-dvh

--- app/app/projects/layout.tsx
+++ app/app/projects/layout.tsx
@@ -8,7 +8,7 @@ export default function ProjectsLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-dvh">
       <AppNavbar />
       <main className="flex-1">{children}</main>
     </div>
