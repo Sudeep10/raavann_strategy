31f77dcfc25ef614b8f6afb73820f199b6f12ac1
Sudeep10 <shettysudeep1010@gmail.com>
2025-11-24 00:38:23 +0530

    Load analytics properly, it was not being fetched sometimes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,13 +27,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <script
-          defer
-          src="https://analytics.raavann.com/script.js"
-          data-website-id="b62d2055-f35b-4f9d-b86a-4713ee15f2de"
-        ></script>
-      </head>
+      <Script
+        defer
+        src="https://analytics.raavann.com/script.js"
+        data-website-id="b62d2055-f35b-4f9d-b86a-4713ee15f2de"
+      />
       <body
         className={`font-sans ${geistSans.variable} ${geistMono.variable} antialiased scroll-smooth`}
       >
