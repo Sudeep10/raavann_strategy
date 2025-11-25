9b293a33df02443eab3dc2462171925c76de0b34
Sudeep10 <shettysudeep1010@gmail.com>
2025-11-25 16:15:52 +0530

    Use proxy instead of middleware

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const url = request.nextUrl.clone();
   if (url.pathname.startsWith("/ph")) {
     const hostname = url.pathname.startsWith("/ph/static/")
